3baadaba692553cd9ecb519d676de430989052c6
Erika Sy <ebasy22@gmail.com>
2025-03-24 10:17:44 -0500

    Add files via upload

--- personalWebsite/email.php
+++ personalWebsite/email.php
@@ -1,34 +1,38 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect the form data
+    // Implement CSRF check here (if applicable)
+
     $name = strip_tags(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = strip_tags(trim($_POST["message"]));
 
-    // Specify your email and subject
-    $recipient = "ebasy22@gmail.com"; // Change this to your email address
+    // Validate inputs
+    if (empty($name) || !filter_var($email, FILTER_VALIDATE_EMAIL) || empty($message)) {
+        http_response_code(400);
+        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+        exit;
+    }
+
+    $recipient = "ebasy22@gmail.com"; // Your email address
     $subject = "New contact from $name";
 
-    // Build the email content
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n\n";
     $email_content .= "Message:\n$message\n";
 
-    // Build the email headers
-    $email_headers = "From: $name <$email>";
+    // Use your email address in the From field
+    $email_headers = "From: YourWebsiteName <your@email.com>\r\n";
+    $email_headers .= "Reply-To: $name <$email>\r\n";
+    $email_headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    // Send the email
     if (mail($recipient, $subject, $email_content, $email_headers)) {
-        // Set a 200 (okay) response code
         http_response_code(200);
         echo "Thank You! Your message has been sent.";
     } else {
-        // Set a 500 (internal server error) response code
         http_response_code(500);
         echo "Oops! Something went wrong, and we couldn't send your message.";
     }
 } else {
-    // Not a POST request, set a 403 (forbidden) response code
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
 }
